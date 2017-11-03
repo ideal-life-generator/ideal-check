379432ea95d9650e3de2a898072098d131cdf238
Tkachenko Vladislav <ideal.life.generator@gmail.com>
2017-11-03 18:13:42 +0200

    Tests and docs fixing

--- test/helpers/validations.js
+++ test/helpers/validations.js
@@ -3,5 +3,5 @@ import isEmail from 'validator/lib/isEmail'
 export const required = value => !value && 'This field is required'
 export const minLength = min => value => value.length < min && `Must be ${min} characters or more`
 export const maxLength = max => value => value.length > max && `Must be not more ${max} characters`
-export const validEmail = value => !isEmail(value) && 'This email is invalid'
-export const samePassword = (value, secondValue) => value !== secondValue && 'This password is different'
+export const validEmail = email => !isEmail(email) && 'This email is invalid'
+export const samePassword = (password, confirmPassword) => password !== confirmPassword && 'This password is different'
